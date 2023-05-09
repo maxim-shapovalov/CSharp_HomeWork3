aa9d0a96f89caf0ad7f999bd34127bf482880ff1
Maxim Shapovalov <Maxim.A.Shapovalov@gmail.com>
2023-05-09 17:15:25 +0300

    finish task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите целое пятизначное число ");
+string x = Console.ReadLine();
+//string x = "12321";
+
+// Проверка на пятизначность
+bool checkUnits(string value)
+{
+    if (value.Length == 5)
+    {
+        return true;
+    }
+    Console.WriteLine("Введенное число не пятизначное");
+    return false;
+}
+
+// Проверка на палиндром
+void checkString(string value)
+{
+    if((value[0] == value[4]) && (value[1] == value[3]))
+    {
+        Console.Write("Введенное число - палиндром");
+    }
+    else
+    {
+        Console.Write("Введенное число - не палиндром");
+    }
+}
+
+if (checkUnits(x)) {
+    checkString(x);
+}
