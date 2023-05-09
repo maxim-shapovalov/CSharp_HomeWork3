363ad8e31424ff0133ccb4330b0ee7e8deb486e4
Maxim Shapovalov <Maxim.A.Shapovalov@gmail.com>
2023-05-09 17:58:40 +0300

    finish task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите положительное целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void Calculate(int c)
+{
+    int result = c * c * c;
+    Console.Write(" " + result);
+}
+
+for (int i = 0; i < x; i++)
+{
+    Calculate(i);
+} 
